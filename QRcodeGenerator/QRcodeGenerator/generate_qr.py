6af30d8f2902358a54930a1fe57842144b74bd67
CalvinH <grave.determination@gmail.com>
2022-12-13 06:34:37 -0500

    Finished the QR code generator

--- QRcodeGenerator/QRcodeGenerator/generate_qr.py
+++ QRcodeGenerator/QRcodeGenerator/generate_qr.py
@@ -1,13 +1,20 @@
-import qrcode
+import qrcode  # Imports a Python library
 
 
-def create_qr_code(string):
-    qr = qrcode.QRcode(
+def create_qr_code(string):  # TLDR: uses tje qrcode class in a function that takes a string
+    # Sets constraints for the object
+    qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
         box_size=10,
-        border=3
+        border=4,
     )
-
     qr.add_data(string)
     qr.make(fit=True)
+
+    # Sets the colors and saves the image into current directory
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("someQRcode.png")
+
+
+create_qr_code("https://www.protondb.com/")

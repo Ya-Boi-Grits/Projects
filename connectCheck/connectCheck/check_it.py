e1cff1355c789e622902844b5498fa90feb8f2ba
CalvinH <grave.determination@gmail.com>
2022-12-14 07:30:50 -0500

    Update check_it.py
    Duplicated the code using a different Python library

--- connectCheck/connectCheck/check_it.py
+++ connectCheck/connectCheck/check_it.py
@@ -1,15 +1,31 @@
-# A library designed for working with urls. [.request] specified to work with the response
+import requests
 import urllib.request as urllib
+# A library designed for working with urls. [.request] specified to work with the response
 
 
 def initialized(url):
     response = urllib.urlopen(url)  # Opens the url
     code = response.getcode()  # Grabs the response code
     if code == 200:
-        return print(f"Connection to {url} was successful")
+        return print(f"Connection to {url} was successful. Code {code}")
     else:
         return print(f"Could not connect to {url}. Code {code}")
 
 
 # Simple function call
 initialized("https://www.google.com/")
+
+
+# ------------- The same thing but with [requests] library instead of [urllib] ---------
+
+
+def init2(url):
+    request = requests.get(url)
+    code = request.status_code
+    if code == 200:
+        return print(f"Connection to {url} was successful. Code {code}")
+    else:
+        return print(f"Error. Could not connect to {url}. Code {code}")
+
+
+init2("https://wiki.mabinogiworld.com/view/Erg")
